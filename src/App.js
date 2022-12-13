3ed8027ef0c2786b7f9b11d29896920ad17a2bef
aydenote <dslgpgh@gmail.com>
2022-12-13 13:50:07 +0900

    로그인 폼 마크업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Join from "./pages/Join/Join";
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  
+  *{
+    box-sizing: border-box;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+  
+  button{
+    margin: 0;
+    padding: 0;
+    border: none;
+    cursor: pointer;
+  }
+`;
+
+const App = () => {
   return (
-
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          {/* <Route path="/join" element={<Join />}></Route> */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
